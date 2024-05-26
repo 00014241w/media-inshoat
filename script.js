e2983def504af01b9c485b7339b2aaa8a3d2f199
00014241w <ernazarovahulkar@gmail.com>
2024-05-26 19:32:02 +0500

    fix carousel section
    Improved carousel hero section, added our advantages and projects sections

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const list = document.querySelector(".carousel-list");
-const item = document.querySelector(".item");
-const itemWidth = list.offsetWidth;
-const maxItems = document.querySelectorAll(".carousel-list li").length;
+// const list = document.querySelector(".carousel-list");
+// const item = document.querySelector(".item");
+// const itemWidth = list.offsetWidth;
+// const maxItems = document.querySelectorAll(".carousel-list li").length;
 
-let currentPosition = 1;
+// let currentPosition = 1;
 
-function handleClick(direction) {
-  if (direction === "previous") {
-    list.scrollBy({ left: -itemWidth, behavior: "smooth" });
-    currentPosition--;
-    if (currentPosition <= 0) {
-      currentPosition = maxItems;
-      list.scrollTo({ left: itemWidth * maxItems, behavior: "smooth" });
-    }
-  } else {
-    list.scrollBy({ left: itemWidth, behavior: "smooth" });
-    currentPosition++;
-    if (currentPosition > maxItems) {
-      currentPosition = 1;
-      list.scrollTo({ left: 0, behavior: "smooth" });
-    }
-  }
-}
+// function handleClick(direction) {
+//   if (direction === "previous") {
+//     list.scrollBy({ left: -itemWidth, behavior: "smooth" });
+//     currentPosition--;
+//     if (currentPosition <= 0) {
+//       currentPosition = maxItems;
+//       list.scrollTo({ left: itemWidth * maxItems, behavior: "smooth" });
+//     }
+//   } else {
+//     list.scrollBy({ left: itemWidth, behavior: "smooth" });
+//     currentPosition++;
+//     if (currentPosition > maxItems) {
+//       currentPosition = 1;
+//       list.scrollTo({ left: 0, behavior: "smooth" });
+//     }
+//   }
+// }
